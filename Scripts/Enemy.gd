73881fcd2103d28dd22d897f90092683df1b4594
mikkel <mikkelpaulick@hotmail.com>
2024-03-24 11:00:27 +0100

    no more

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,6 +3,12 @@ extends RigidBody2D
 signal health_depleted
 signal health_changed(old_value: int, new_value: int)
 
+var bruh1 = preload("res://Textures/Skelarcher_sprite.png")
+var bruh2 = preload("res://WIZARD_KING_32x32.png")
+
+@export var king: bool
+@export var thing: Sprite2D
+
 var health: int = 10
 
 func take_damage(amount: int):
@@ -16,6 +22,9 @@ func take_damage(amount: int):
 
 # Called when the node enters the scene tree for the first time.
 func _init():
+	#if king:
+	#	thing.texture = bruh2
+	#thing.texture = bruh1
 	contact_monitor = true
 	max_contacts_reported = 1
 
